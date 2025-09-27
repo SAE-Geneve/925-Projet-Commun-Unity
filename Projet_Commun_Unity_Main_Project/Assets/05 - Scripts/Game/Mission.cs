9970391dd0ddec5518e6bf421c0f69d823b75fd8
Jemo <103411555+Jemow@users.noreply.github.com>
2025-09-27 14:34:09 +0200

    Starting mission proto script

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Game/Mission.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Game/Mission.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 
 public class Mission : MonoBehaviour
 {
-
+    [Header("Parameters")] 
+    [SerializeField] private string _name = "New Mission";
+    [SerializeField] private float _initialTime = 30f;
+    [SerializeField] private bool _locked;
 }

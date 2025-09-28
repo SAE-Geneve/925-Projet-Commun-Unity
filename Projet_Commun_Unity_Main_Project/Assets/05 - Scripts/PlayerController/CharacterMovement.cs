8eea48c2a2ded33253a840a51a7edda872cf41b1
hugo zeidan <hugzeidan@outlook.com>
2025-09-28 12:53:11 +0200

    Changed Inputs, Fixed the jump and Factorized CharacterMovement script

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/PlayerController/CharacterMovement.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/PlayerController/CharacterMovement.cs
@@ -2,27 +2,116 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    [Header("Physic")]
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpHeight = 1.5f;
     
+    [Header("GroundCheck")]
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private float groundCheckDistance = 0.4f;
-
+    
     private Rigidbody _rb;
-    private InputManager _inputs;
-
-    private bool _isGrounded;
-    private bool _wasGrounded;
+    
     private readonly float _gravity = -20f;
-
+    
+    private bool _isGrounded;
+    private bool _jumpQueued;
+    
+    // Camera Directions
     private Vector3 _camForward;
     private Vector3 _camRight;
-
+    
+    // Inputs
+    private bool _isJumping;
+    private Vector2 _moveInput;
+    
+    public bool IsJumping { get => _isJumping; set => _isJumping = value; }
+    public Vector2 MoveInput { get => _moveInput; set => _moveInput = value; }
+    
+    
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        _inputs = GetComponent<InputManager>();
+        GetCameraDirections();
+    }
+    
+    void Update()
+    {
+        RotateCharacter();
+
+        // Queue jump when input is pressed
+        if (_isJumping)
+        {
+            _jumpQueued = true;
+        }
+        else
+        {
+            _jumpQueued = false;
+        }
+    }
+    
+    void FixedUpdate()
+    {
+        GroundCheck();
+        HorizontalMovement();
+        JumpLogic();
+
+        // Apply extra gravity manually for snappier jumps
+        _rb.AddForce(Vector3.up * _gravity, ForceMode.Acceleration);
+    }
+    
+    private void GroundCheck()
+    {
+        // Ground check using Raycast
+        _isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundMask);
+        // Debug Ray
+        Debug.DrawRay(transform.position, Vector3.down * groundCheckDistance, _isGrounded ? Color.green : Color.red);
+    }
+    
+    private void HorizontalMovement()
+    {
+        // Calculate the change in velocity needed and apply it (no acceleration or deceleration)
+        Vector3 moveDir = _camRight * _moveInput.x + _camForward * _moveInput.y;
         
+        Vector3 targetVelocity = moveDir * speed;
+        Vector3 velocity = _rb.linearVelocity;
+        
+        Vector3 velocityChange = targetVelocity - new Vector3(velocity.x, 0, velocity.z);
+        
+        _rb.AddForce(velocityChange, ForceMode.VelocityChange);
+    }
+    
+    void Jump()
+    {
+        // Reset vertical velocity once before the jump
+        _rb.linearVelocity = new Vector3(_rb.linearVelocity.x, 0f, _rb.linearVelocity.z);
+        
+        float jumpVelocity = Mathf.Sqrt(jumpHeight * -2f * _gravity);
+        _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.VelocityChange);
+    }
+
+    private void JumpLogic()
+    {
+        // If jump was queued and player is grounded, perform jump
+        if (_jumpQueued && _isGrounded)
+        {
+            Jump();
+            _jumpQueued = false; 
+        }
+    }
+
+    private void RotateCharacter()
+    {
+        // Rotate character to face move direction
+        Vector3 moveDir = _camRight * _moveInput.x + _camForward * _moveInput.y;
+        if (moveDir != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDir), 0.15f);
+        }
+    }
+    
+    private void GetCameraDirections()
+    {
         // Calculate directions relative to the camera (static camera)
         _camForward = Camera.main.transform.forward;
         _camForward.y = 0;
@@ -31,50 +120,5 @@ public class CharacterMovement : MonoBehaviour
         _camRight = Camera.main.transform.right;
         _camRight.y = 0;
         _camRight.Normalize();
-    }
-
-    void Update()
-    {
-        // Ground check using Raycast
-        _isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundMask);
-
-        // Jump logic (trigger once when landing if button held)
-        if (_inputs.JumpInput && _isGrounded && !_wasGrounded)
-        {
-            Jump();
-        }
-
-        _wasGrounded = _isGrounded;
-
-        // Rotate character to face move direction
-        Vector3 moveDir = _camRight * _inputs.MoveInput.x + _camForward * _inputs.MoveInput.y;
-        if (moveDir != Vector3.zero)
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDir), 0.15f);
-        }
-    }
-
-    void FixedUpdate()
-    {
-        // Calculate the change in velocity needed and apply it (no acceleration or deceleration)
-        Vector3 moveDir = _camRight * _inputs.MoveInput.x + _camForward * _inputs.MoveInput.y;
-        
-        Vector3 targetVelocity = moveDir * speed;
-        Vector3 velocity = _rb.linearVelocity;
-        
-        Vector3 velocityChange = targetVelocity - new Vector3(velocity.x, 0, velocity.z);
-        _rb.AddForce(velocityChange, ForceMode.VelocityChange);
-
-        // Apply extra gravity manually for snappier jumps
-        _rb.AddForce(Vector3.up * _gravity, ForceMode.Acceleration);
-    }
-
-    void Jump()
-    {
-        // Reset vertical velocity once before the jump
-        _rb.linearVelocity = new Vector3(_rb.linearVelocity.x, 0f, _rb.linearVelocity.z);
-        
-        float jumpVelocity = Mathf.Sqrt(jumpHeight * -2f * _gravity);
-        _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.VelocityChange);
     }
 }
--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/PlayerController/InputManager.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/PlayerController/InputManager.cs
@@ -3,12 +3,13 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    private Vector2 _moveInput = Vector2.zero;
-    private bool _jumpInput;
+    private CharacterMovement _characterMovement;
 
-    public Vector2 MoveInput => _moveInput;
-    public bool JumpInput => _jumpInput;
+    private void Start()
+    {
+        _characterMovement = GetComponent<CharacterMovement>();
+    }
 
-    public void OnMove(InputAction.CallbackContext context) => _moveInput = context.ReadValue<Vector2>();
-    public void OnJump(InputAction.CallbackContext context) => _jumpInput = context.ReadValueAsButton();
+    public void OnMove(InputAction.CallbackContext context) => _characterMovement.MoveInput = context.ReadValue<Vector2>();
+    public void OnJump(InputAction.CallbackContext context) => _characterMovement.IsJumping = context.ReadValueAsButton();
 }

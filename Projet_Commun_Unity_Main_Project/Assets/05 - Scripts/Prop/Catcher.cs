68644c826d3a2ecae44555518236bb46ac237d30
benjamin layson <benjaminlayson0@gmail.com>
2025-10-06 12:45:30 +0200

    started to implement the "pingPong" throw force

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Prop/Catcher.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Prop/Catcher.cs
@@ -9,27 +9,50 @@ public class Catcher : MonoBehaviour
     private IGrabbable _grabbed;
     private float grabStartTime;
     [SerializeField] private float throwThreshold = 1.0f;
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float maxThrowForce = 10f;
     Vector3 throwDirection;
+    float heldTime;
+    private float throwPower;
+    private bool isCharging;
 
     public void CatchInput(InputAction.CallbackContext context)
     {
         if (context.started)
         {
             grabStartTime = Time.time;
+            isCharging = true;
         }
         if (context.canceled)
         {
-            float heldTime = Time.time - grabStartTime;
-            Vector3 forward = transform.forward;
-            Vector3 upward = transform.up * 0.5f;
-            throwDirection = (forward + upward).normalized * heldTime * 6;
+            heldTime = Time.time - grabStartTime;
+            isCharging = false;
         }
         if (_grabbed != null)
         {
-            _grabbed.Dropped(throwDirection);
+            _grabbed.Dropped(ThrowDirection());
         }
         else TryGrab();
     }
+
+    private void Update()
+    {
+        if (isCharging)
+        {
+            throwPower = Mathf.PingPong((Time.time - grabStartTime) * 2, 1f);
+        }
+    }
+    
+    private Vector3 ThrowDirection()
+    {
+        Vector3 forward = transform.forward;
+        Vector3 upward = transform.up * 0.5f;
+        float force = throwPower * maxThrowForce;
+        throwDirection = (forward + upward).normalized * force;
+        
+        return throwDirection + _rb.linearVelocity;
+    }
+   
     
     private void TryAction<T>(Action<T> onFound) where T : class
     {
@@ -38,9 +61,9 @@ public class Catcher : MonoBehaviour
         foreach (var hit in hits)
         {
             if (!hit.TryGetComponent(out T component)) continue;
-
             onFound(component);
             break;
+            
         }
     }
     

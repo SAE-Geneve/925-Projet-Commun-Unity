f94ff8f9c9cdadf489dc99dd2850f12899dc523d
Jemo <103411555+Jemow@users.noreply.github.com>
2025-10-12 22:04:32 +0200

    Implemented Conveyor Belt movement woohoo

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Mission/Conveyor Belt/ConveyorBelt.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Mission/Conveyor Belt/ConveyorBelt.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 public class ConveyorBelt : MonoBehaviour
 {
     [Header("Parameters")] 
-    [SerializeField] private Vector3 _direction;
-    [SerializeField] private float _speed;
+    [SerializeField] private Vector3 _direction = Vector3.forward;
+    [SerializeField] private float _speed = 7f;
     
-    private List<Prop> _props = new();
+    private readonly List<Prop> _props = new();
 
     private void FixedUpdate()
     {
         foreach (Prop prop in _props)
-        {
-            
-        }
+            prop.Rb.AddForce(_direction * _speed, ForceMode.Force);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        GameObject obj = other.gameObject;
+        if(obj.tag != "Prop" || !obj.TryGetComponent(out Prop prop)) return;
+        
+        _props.Add(prop);
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        GameObject obj = other.gameObject;
+        if(obj.tag != "Prop" || !obj.TryGetComponent(out Prop prop)) return;
+        
+        _props.Remove(prop);
     }
 }

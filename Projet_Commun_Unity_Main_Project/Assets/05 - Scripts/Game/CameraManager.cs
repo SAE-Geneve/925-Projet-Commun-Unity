f0290cc13a61f78e543c4dd4c8230daa0a37ae40
hugo zeidan <hugzeidan@outlook.com>
2025-10-20 17:32:48 +0200

    made a prototype Hub scene to test camera zoom

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Game/CameraManager.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Game/CameraManager.cs
@@ -1,36 +1,57 @@
 using System;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using Unity.AppUI.UI;
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.Splines;
 
 public class CameraManager : MonoBehaviour
 {
     private CinemachineCamera _cam;
-    private float _baseFOV;
+    
+    [SerializeField] private float minFOV = 60f;
+    [SerializeField] private float maxFOV = 80f;
+    [SerializeField] private float zoomMultiplier = 1.5f;
+    
     [SerializeField] private PlayerManager playerManager;
     [SerializeField] private Transform targetPosition;
+
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _cam = GetComponent<CinemachineCamera>();
-        _baseFOV = _cam.Lens.FieldOfView;
+        _cam.Lens.FieldOfView = minFOV;
     }
     
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (playerManager.Players.Count == 0)
+        if (playerManager.Players.Count <= 0) return;
+        targetPosition.position = GetMeanVector();
+
+        if (minFOV + GetSpacing() <= maxFOV)
         {
-            
-        }
-        else
-        {
-            targetPosition.position = GetMeanVector();
+            _cam.Lens.FieldOfView = minFOV + GetSpacing() * zoomMultiplier;
         }
     }
 
+    private float GetSpacing()
+    {
+        float spacing = 0;
+        int playerCount = playerManager.Players.Count;
+        
+        foreach (var pl in playerManager.Players)
+        {
+            Vector3 pos = pl.transform.position;
+            
+            spacing += Mathf.Abs((pos.x - targetPosition.position.x)) + Mathf.Abs((pos.z - targetPosition.position.z));
+        }
+        return spacing / playerCount;
+    }
+    
     private Vector3 GetMeanVector(){
         int playerCount = playerManager.Players.Count;
         

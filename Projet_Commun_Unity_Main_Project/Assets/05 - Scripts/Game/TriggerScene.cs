1735990ab1df376b5f897e325050186cce90deb5
Jemo <103411555+Jemow@users.noreply.github.com>
2025-10-24 16:17:34 +0200

    Fixed on player left scene trigger zone

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Game/TriggerScene.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Game/TriggerScene.cs
@@ -1,23 +1,33 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class TriggerScene : MonoBehaviour
 {
+    [Header("Parameters")]
+    [SerializeField] string _sceneToLoad;
     
-    [SerializeField] string sceneToLoad;
+    
+    
     private int _playerNumber;
+
+    private void Start() => PlayerManager.Instance.OnPlayerRemoved += CheckPlayerNumber;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
         _playerNumber++;
-        if(_playerNumber == PlayerManager.Instance.PlayerCount)
-        {
-            SceneLoader.Instance.LoadScene(sceneToLoad);
-        }
+        CheckPlayerNumber();
     }
 
     private void OnTriggerExit(Collider other)
     {
         if(other.CompareTag("Player"))
             _playerNumber--;
+    }
+
+    private void CheckPlayerNumber()
+    {
+        if(_playerNumber == PlayerManager.Instance.PlayerCount)
+            SceneLoader.Instance.LoadScene(_sceneToLoad);
     }
 }

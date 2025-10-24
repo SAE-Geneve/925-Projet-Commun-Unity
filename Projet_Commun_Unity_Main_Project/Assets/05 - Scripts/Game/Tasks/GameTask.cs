19bd04fc1f13f201aabeb91340ce699fa1fdbecd
Jemo <103411555+Jemow@users.noreply.github.com>
2025-10-25 00:08:10 +0200

    Implemented Behavior Trees with GameTask!

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Game/Tasks/GameTask.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Game/Tasks/GameTask.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -15,6 +16,9 @@ public abstract class GameTask : MonoBehaviour
     public UnityEvent OnSucceed;
     public UnityEvent OnFailed;
 
+    public event Action OnSucceedAction;
+    public event Action OnFailedAction;
+
     protected bool Done;
     
     protected virtual void Start() => OnStart?.Invoke();
@@ -24,6 +28,7 @@ public abstract class GameTask : MonoBehaviour
         if(!_multiple) Done = true;
         
         OnSucceed?.Invoke();
+        OnSucceedAction?.Invoke();
         
         Debug.Log($"Task {_taskName} done!");
     }
@@ -31,6 +36,7 @@ public abstract class GameTask : MonoBehaviour
     protected void Failed()
     {
         OnFailed?.Invoke();
+        OnFailedAction?.Invoke();
         
         Debug.Log($"Task {_taskName} failed!");
     }

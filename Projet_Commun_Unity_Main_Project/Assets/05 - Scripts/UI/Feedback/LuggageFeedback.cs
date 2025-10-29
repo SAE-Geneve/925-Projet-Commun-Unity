9c05513ce11bb837406a77fb9acd6dfbe5e205db
StylishBoi <max@rdh.ch>
2025-10-29 15:09:50 +0100

    - Created new 'UIFeedback' universal script | - Created new 'LuggageFeedback' script | - Implemented base UI for 'Border Control' scene

---
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/UI/Feedback/LuggageFeedback.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LuggageFeedback : MonoBehaviour
+{
+    [SerializeField] private float effectDuration=2.5f;
+    
+    [Header("Reaction Images")]
+    [SerializeField] private Image happyImage;
+    [SerializeField] private Image unhappyImage;
+    
+    private AudioManager _audioManager;
+    private UIFeedback _uiFeedback;
+    
+    private void Start()
+    {
+        _audioManager = AudioManager.Instance;
+        _uiFeedback = GetComponent<UIFeedback>();
+    }
+    public void HappyResult()
+    {
+        _audioManager.PlaySfx(_audioManager.successSFX);
+        StartCoroutine(_uiFeedback.ImageFade(happyImage, effectDuration));
+    }
+    public void UnhappyResult()
+    {
+        _audioManager.PlaySfx(_audioManager.failureSFX);
+        StartCoroutine(_uiFeedback.ImageFade(unhappyImage, effectDuration));
+    }
+}

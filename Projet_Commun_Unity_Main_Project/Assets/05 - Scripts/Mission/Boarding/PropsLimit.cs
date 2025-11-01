f71433e65922bbced18edd2805c0fbfa766bd0b9
hugo zeidan <hugzeidan@outlook.com>
2025-11-01 13:19:27 +0100

    boardind stairs doesn't destroy anymore and conveyor accept 20 luggage

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Mission/Boarding/PropsLimit.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Mission/Boarding/PropsLimit.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class PropsLimit : MonoBehaviour
 {
 
-    private int _luggageCounter = 0;
+    [SerializeField] private int _luggageCounter = 0;
     [SerializeField] private int luggageLimit = 20;
     
     private OnePropSpawner[] _spawners;

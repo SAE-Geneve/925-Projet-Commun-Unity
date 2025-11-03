22334fcb068575972b128868e56dd7a159982aed
WIXXEL <97680405+WixXelTV@users.noreply.github.com>
2025-11-03 23:11:09 +0100

    Removed forgotten useless code in AndTask.cs

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Game/Tasks/AndTask.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Game/Tasks/AndTask.cs
@@ -17,35 +17,15 @@ public class AndTask : GameTask
     private void CheckTasks()
     {
         if (tasks.Any(task => !task.Done))
+        {
             return;
-        ResetAllPlayers();
-        // ✅ Toutes les tasks sont terminées
+        }
+
         Succeed();
 
-        // Nettoyage
         foreach (var task in tasks)
         {
             task.OnSucceedAction -= CheckTasks;
-        }
-
-        // ✅ On fait un Reset() sur tous les Player de la scène
-     
-    }
-
-    private void ResetAllPlayers()
-    {
-        // On cherche tous les objets appelés "Player"
-        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-
-        foreach (var player in players)
-        {
-            Controller controller = player.GetComponent<Controller>();
-            if (controller != null)
-            {
-                Debug.LogWarning("DROP ALL PLAYERS");
-                controller.Dropped();
-                controller.Reset();
-            }
         }
     }
 }

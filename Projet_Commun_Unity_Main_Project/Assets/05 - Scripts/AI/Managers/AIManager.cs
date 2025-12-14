cf9a1e370cf57d07dca5724db1a2fd26b782f716
Jemo <103411555+Jemow@users.noreply.github.com>
2025-12-14 21:40:42 +0100

    Implemented new border control AI! youpi

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/AI/Managers/AIManager.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/AI/Managers/AIManager.cs
@@ -9,32 +9,32 @@ public class AIManager : MonoBehaviour
     [SerializeField] protected AIController npcPrefab;
     [SerializeField] protected float spawnInterval = 5f;
 
-    [Header("Debug / Test")]
-    [SerializeField] protected bool spawnEnabled = false;
+    // [Header("Debug / Test")]
+    // [SerializeField] protected bool spawnEnabled;
 
-    private Coroutine spawnRoutine;
+    private Coroutine _spawnRoutine;
 
     private void Update()
     {
         // Détecte changement de checkbox
-        if (spawnEnabled && spawnRoutine == null)
-            StartSpawn();
-        else if (!spawnEnabled && spawnRoutine != null)
-            StopSpawn();
+        // if (spawnEnabled && _spawnRoutine == null)
+        //     StartSpawn();
+        // else if (!spawnEnabled && _spawnRoutine != null)
+        //     StopSpawn();
     }
 
     public void StartSpawn()
     {
-        if (spawnRoutine != null) return;
-        spawnRoutine = StartCoroutine(SpawnLoop());
+        if (_spawnRoutine != null) return;
+        _spawnRoutine = StartCoroutine(SpawnLoop());
     }
 
     public void StopSpawn()
     {
-        if (spawnRoutine != null)
+        if (_spawnRoutine != null)
         {
-            StopCoroutine(spawnRoutine);
-            spawnRoutine = null;
+            StopCoroutine(_spawnRoutine);
+            _spawnRoutine = null;
         }
     }
 
@@ -49,7 +49,7 @@ public class AIManager : MonoBehaviour
 
     protected virtual void SpawnNPC()
     {
-        if (spawnPoints == null || spawnPoints.Count == 0 || npcPrefab == null) return;
+        if (npcPrefab || spawnPoints == null || spawnPoints.Count == 0) return;
 
         Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
         Instantiate(npcPrefab, spawnPoint.position, spawnPoint.rotation);

9220a348c213d50e056c12769266354d1cc75cf6
Jemo <103411555+Jemow@users.noreply.github.com>
2025-12-14 23:35:13 +0100

    Implemented new Lost Luggage

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Prop/PropSpawner.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Prop/PropSpawner.cs
@@ -8,12 +8,17 @@ public class PropSpawner : MonoBehaviour
     [Tooltip("Does the props belongs to a PropManager")]
     [SerializeField] private PropManager _propManager;
 
-    [Header("Parameters")]
+    [Header("Parameters")] 
+    [SerializeField] private bool _interval;
     [SerializeField] private float _spawnDelay = 2f;
 
-    private void Start() => InvokeRepeating(nameof(SpawnProp), _spawnDelay, _spawnDelay);
+    private void Start()
+    {
+        if(_interval)
+            InvokeRepeating(nameof(SpawnProp), _spawnDelay, _spawnDelay);
+    }
 
-    private void SpawnProp()
+    public void SpawnProp()
     {
         Prop prop = Instantiate(_propsToSpawn[Random.Range(0, _propsToSpawn.Length)], transform.position, Quaternion.identity);
         if(_propManager) _propManager.AddProp(prop);

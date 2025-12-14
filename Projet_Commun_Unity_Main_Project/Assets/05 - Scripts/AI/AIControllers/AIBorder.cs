04505a5338cbe0c54ef9294ae8f91e09a8c90946
Jemo <103411555+Jemow@users.noreply.github.com>
2025-12-14 22:55:11 +0100

    Update AIManager code (removed redundant code)

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/AI/AIControllers/AIBorder.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/AI/AIControllers/AIBorder.cs
@@ -1,33 +1,15 @@
-using System.Collections.Generic;
 using UnityEngine;
-using Unity.Behavior;
 
 public class AIBorder : AIController
 {
-    private Transform conveyor;
-    private Transform detector;
-    private Transform throwHere;
-    private Transform exitPoint;
-    private PropManager propManager;
-
     public void Initialize(Transform conv, Transform det, Transform thr, Transform exit, PropManager propM)
     {
-        conveyor = conv;
-        detector = det;
-        throwHere = thr;
-        exitPoint = exit;
-        propManager = propM;
-        ApplyBlackboardValues();
-    }
-    
-    private void ApplyBlackboardValues()
-    {
-        if (_behaviorAgent == null) return;
+        if (!_behaviorAgent) return;
 
-        if (conveyor != null) _behaviorAgent.SetVariableValue("ConveyorBelt", conveyor);
-        if (detector != null) _behaviorAgent.SetVariableValue("Detector", detector);
-        if (throwHere != null) _behaviorAgent.SetVariableValue("ThrowHere", throwHere);
-        if (exitPoint != null) _behaviorAgent.SetVariableValue("Exit", exitPoint);
-        if (propManager != null) _behaviorAgent.SetVariableValue("PropManager", propManager);
+        if (conv) _behaviorAgent.SetVariableValue("ConveyorBelt", conv);
+        if (det) _behaviorAgent.SetVariableValue("Detector", det);
+        if (thr) _behaviorAgent.SetVariableValue("ThrowHere", thr);
+        if (exit) _behaviorAgent.SetVariableValue("Exit", exit);
+        if (propM) _behaviorAgent.SetVariableValue("PropManager", propM);
     }
 }

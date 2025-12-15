14a5d35a75a00361d19149987b66be008fad71ed
WIXXEL <97680405+WixXelTV@users.noreply.github.com>
2025-12-15 14:13:15 +0100

    Added a clear to the ai manager

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/AI/Managers/AIManager.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/AI/Managers/AIManager.cs
@@ -10,19 +10,9 @@ public class AIManager : MonoBehaviour
     [SerializeField] protected float spawnInterval = 5f;
     [SerializeField] private bool _spawnOnStart;
 
-    // [Header("Debug / Test")]
-    // [SerializeField] protected bool spawnEnabled;
-
     private Coroutine _spawnRoutine;
-
-    private void Update()
-    {
-        // Détecte changement de checkbox
-        // if (spawnEnabled && _spawnRoutine == null)
-        //     StartSpawn();
-        // else if (!spawnEnabled && _spawnRoutine != null)
-        //     StopSpawn();
-    }
+    
+    private List<AIController> _spawnedAIs = new List<AIController>();
 
     protected virtual void Start()
     {
@@ -43,6 +33,21 @@ public class AIManager : MonoBehaviour
             _spawnRoutine = null;
         }
     }
+    
+    public void ClearAIs()
+    {
+        StopSpawn();
+        
+        foreach (var ai in _spawnedAIs)
+        {
+            if (ai != null)
+            {
+                Destroy(ai.gameObject);
+            }
+        }
+        
+        _spawnedAIs.Clear();
+    }
 
     private IEnumerator SpawnLoop()
     {
@@ -55,9 +60,10 @@ public class AIManager : MonoBehaviour
 
     protected virtual void SpawnNPC()
     {
-        if (npcPrefab || spawnPoints == null || spawnPoints.Count == 0) return;
+        if (!npcPrefab || spawnPoints == null || spawnPoints.Count == 0) return;
 
         Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
-        Instantiate(npcPrefab, spawnPoint.position, spawnPoint.rotation);
+        AIController newNpc = Instantiate(npcPrefab, spawnPoint.position, spawnPoint.rotation);
+        _spawnedAIs.Add(newNpc);
     }
 }

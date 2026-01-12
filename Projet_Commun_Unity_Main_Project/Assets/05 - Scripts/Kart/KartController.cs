36adbaaab50b6a37c26b97efbc2c08675624a962
benjamin layson <benjaminlayson0@gmail.com>
2026-01-12 11:50:56 +0100

    updating the kart bug : the player is now scaled correcly when exiting the kart, and it desapear when driving the kart.

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Kart/KartController.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Kart/KartController.cs
@@ -16,8 +16,23 @@ public class KartController : MonoBehaviour, IInteractable
 
     public void Interact(PlayerController playerController)
     {
+        var playerRb = playerController.GetComponent<Rigidbody>();
+        var playerCol = playerController.GetComponent<Collider>();
+        
+        if (playerRb) playerRb.isKinematic = true;
+        if (playerCol) playerCol.enabled = false;
+        
+        Renderer[] playerRenderers = playerController.GetComponentsInChildren<Renderer>();
+        foreach (var r in playerRenderers)
+        {
+            r.enabled = false;
+        }
+        
         playerController.transform.SetParent(seatPosition);
-        playerController.transform.position = seatPosition.position;
+        //playerController.transform.position = seatPosition.position;
+        playerController.transform.localPosition = Vector3.zero;
+        playerController.transform.localRotation = Quaternion.identity;
+        playerController.transform.localScale = Vector3.one;
         
         playerController.Input.SwitchCurrentActionMap("Kart");
         playerController.KartController = this;
@@ -33,11 +48,31 @@ public class KartController : MonoBehaviour, IInteractable
     {
         playerController.transform.SetParent(PlayerManager.Instance.transform);
         
+        Renderer[] playerRenderers = playerController.GetComponentsInChildren<Renderer>();
+        foreach (var r in playerRenderers)
+        {
+            r.enabled = true;
+        }
+        
+        var playerRb = playerController.GetComponent<Rigidbody>();
+        var playerCol = playerController.GetComponent<Collider>();
+        
+        if (playerRb) 
+        {
+            playerRb.isKinematic = false;
+            playerRb.linearVelocity = Vector3.zero; 
+            playerRb.angularVelocity = Vector3.zero;
+        }
+        if (playerCol) playerCol.enabled = true;
+        
+        playerController.transform.localScale = Vector3.one;
+        
         playerController.Input.SwitchCurrentActionMap("game");
         playerController.KartController = null;
         playerController.KartMovement = null;
 
         playerController.transform.position = transform.position + transform.right * 2f;
+        playerController.transform.rotation = Quaternion.identity;
         
         playerController.OnExitKart?.Invoke();
     }

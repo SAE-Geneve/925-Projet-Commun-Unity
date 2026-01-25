82afb48de9eff8a21afd3bab305ae501144742c3
Jemo <103411555+Jemow@users.noreply.github.com>
2026-01-25 15:45:14 +0100

    Added general timer display

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/UI/Menus/HudUI.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/UI/Menus/HudUI.cs
@@ -1,15 +1,27 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class HudUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI totalMoneyScore;
     [SerializeField] private TextMeshProUGUI totalTimeScore;
 
+    private GameManager _gameManager;
+
+    private void Start() => _gameManager = GameManager.Instance;
+
     public void UpdateHUDUI()
     {
         totalMoneyScore.text = ScoreSystem.TotalMoneyScore+"$";
-        totalTimeScore.text = ScoreSystem.TotalTimeCollected+"s";
+        // totalTimeScore.text = ScoreSystem.TotalTimeCollected+"s";
     }
+
+    private void Update()
+    {
+        int minutes = Mathf.FloorToInt(_gameManager.Timer / 60f);
+        int seconds = Mathf.FloorToInt(_gameManager.Timer % 60f);
+
+        totalTimeScore.SetText($"{minutes:00}:{seconds:00}");
+    }
+
 }

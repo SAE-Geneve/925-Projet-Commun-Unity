3454261e1880024918e7837259ac18db7fd4dcf6
StylishBoi <max@rdh.ch>
2026-01-28 13:55:25 +0100

    - Enhanced passport UI | - Made UI screen facing 'script'

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/UI/UIBillboard.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/UI/UIBillboard.cs
@@ -9,7 +9,7 @@ public class UIBillboard : MonoBehaviour
     private void LateUpdate()
     {
         Vector3 direction = transform.position - Camera.main.transform.position;
-        direction.y = 0;
+        direction.z = 0;
         transform.rotation = Quaternion.LookRotation(direction);
     }
 }

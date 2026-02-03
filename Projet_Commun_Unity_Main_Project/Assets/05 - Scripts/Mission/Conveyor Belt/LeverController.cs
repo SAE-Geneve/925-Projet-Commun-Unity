65a4fdf3f4cc6de007698254f1f35fb017ab9f9f
WIXXEL <97680405+WixXelTV@users.noreply.github.com>
2026-02-03 15:42:24 +0100

    Now you can't interact with the lever in the boarding zone until the mission has started.

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Mission/Conveyor Belt/LeverController.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Mission/Conveyor Belt/LeverController.cs
@@ -6,6 +6,9 @@ public class LevierController : MonoBehaviour
     [Header("Réglages")]
     public Animator animator;
     
+    [Tooltip("Définit si le levier peut être utilisé ou non.")]
+    public bool canInteract = true; 
+    
     [Header("Connexions")]
     public AIManager aiManager; 
 
@@ -13,28 +16,43 @@ public class LevierController : MonoBehaviour
     [NonSerialized]public string triggerOff = "Desactivate";
     
     private bool isActive = false;
-    
     public bool IsActive => isActive; 
     
     public void ChangeState()
     {
-        isActive = !isActive;
+        if (!canInteract) return; 
 
+        isActive = !isActive;
+        UpdateVisualsAndLogic();
+    }
+
+    private void UpdateVisualsAndLogic()
+    {
         if (isActive)
         {
             animator.SetTrigger(triggerOn);
-            if (aiManager != null)
-            {
-                aiManager.StartSpawn();
-            }
+            if (aiManager != null) aiManager.StartSpawn();
         }
         else
         {
             animator.SetTrigger(triggerOff);
-            if (aiManager != null)
-            {
-                aiManager.StopSpawn();
-            }
+            if (aiManager != null) aiManager.StopSpawn();
+        }
+    }
+
+    public void UnlockLever()
+    {
+        canInteract = true;
+    }
+
+    public void DeactivateAndLock()
+    {
+        canInteract = false;
+
+        if (isActive)
+        {
+            isActive = false;
+            UpdateVisualsAndLogic();
         }
     }
 }

a33a5892d9150e97f19c90bc82a543185e300808
Jemo <103411555+Jemow@users.noreply.github.com>
2026-02-21 16:49:43 +0100

    Imported font and started new Score Board after finishing mission

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/UI/UIManager.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/UI/UIManager.cs
@@ -1,12 +1,18 @@
+using System.Collections;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private Canvas _pauseCanvas;
+    [SerializeField] private Canvas _scoreBoardCanvas;
+
+    [Header("Parameters")] 
+    [SerializeField] private float scoreBoardTime = 5f;
+    
     public static UIManager Instance { get; private set; }
     
-    // public MissionID TargetMission { get; set; }
-    
-    [SerializeField] private Canvas _pauseCanvas;
+    private GameManager _gameManager;
 
     public void ShowPauseCanvas(bool state) => _pauseCanvas.gameObject.SetActive(state);
     
@@ -15,5 +21,22 @@ public class UIManager : MonoBehaviour
     {
         if (Instance && Instance != this) Destroy(gameObject);
         else Instance = this;
+    }
+
+    private void Start() => _gameManager = GameManager.Instance;
+
+    public void DisplayScoreBoard()
+    {
+        if(_gameManager.State != GameState.Playing) return;
+        StartCoroutine(ScoreBoardRoutine());
+    }
+
+    private IEnumerator ScoreBoardRoutine()
+    {
+        _gameManager.SwitchState(GameState.Cinematic);
+        _scoreBoardCanvas.enabled = true;
+        yield return new WaitForSeconds(scoreBoardTime);
+        _scoreBoardCanvas.enabled = false;
+        _gameManager.SwitchState(GameState.Playing);
     }
 }

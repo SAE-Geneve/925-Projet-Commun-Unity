25347257c96c1e78c41026a5fdec49da58d3a2ba
StylishBoi <max@rdh.ch>
2026-02-23 20:18:35 +0100

    - Started creating new minigame prefab

---
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/UI/Mini-Games/NewMinigameUI.cs
@@ -0,0 +1,120 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class NewMinigameUI : MonoBehaviour
+{
+    //public bool ExtraScores;
+    
+    [Header("Score Texts")]
+    [SerializeField] private TextMeshProUGUI totalScore;
+    private int _totalScore;
+    
+    [SerializeField] private GameObject[] scorePanel;
+    [SerializeField] private TextMeshProUGUI[] playerScoreText;
+    private int[] _playerScore;
+    
+    private ScoreManager _scoreManager;
+    
+    [Header("Total Score Effects")]
+    [SerializeField] private Image scoreImageFade;
+    [SerializeField] private TextMeshProUGUI scoreTextFade;
+    
+    [Header("Secondary Score Image Effects")]
+    [SerializeField] private Image leftSubScoreImage;
+    [SerializeField] private Image rightSubscoreImage;
+    
+    [Header("Secondary Score Text Effects")]
+    [SerializeField] private TextMeshProUGUI subScoreEffect1;
+    [SerializeField] private TextMeshProUGUI subScoreEffect2;
+    [SerializeField] public TextMeshProUGUI subScoreEffect3;
+    [SerializeField] public TextMeshProUGUI subScoreEffect4;
+    
+    private UIScreenEffects _uiScreenEffects;
+    
+    private void Start()
+    {
+        _scoreManager = GameManager.Instance.Scores;
+
+        for (int i = 0; i < 4; i++)
+        {
+            _playerScore[i] = 0;
+        }
+        if (TryGetComponent(out _uiScreenEffects))
+        {
+            Debug.Log("Found UI Text Effects");
+            if (scoreImageFade != null)
+            {
+                _uiScreenEffects.ImagePoolCreation(scoreImageFade);
+            }
+            else
+            {
+                Debug.LogWarning($"[{name}] BaseMinigameUI : 'scoreImageFade' est manquant !");
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"[{name}] BaseMinigameUI : Composant 'UIScreenEffects' manquant sur cet objet !");
+        }
+    }
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < PlayerManager.Instance.Players.Count; i++)
+        {
+            scorePanel[i].SetActive(true);
+        }
+    }
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < PlayerManager.Instance.Players.Count; i++)
+        {
+            scorePanel[i].SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        for (int x = 0; x < PlayerManager.Instance.Players.Count; x++)
+        {
+            if (_playerScore[x] != _scoreManager.MissionScores[x])
+            {
+                PlayerScoreIncrease(x, _scoreManager.MissionScores[x]);
+            }
+        }
+        //
+        // if (_totalScore != ScoreSystem.TotalMinigameScore)
+        // {
+        //     TotalScoreIncrease();
+        // }
+    }
+    
+    private void TotalScoreIncrease()
+    {
+        if (_uiScreenEffects != null)
+        {
+            StartCoroutine(_uiScreenEffects.DoImagePoolFade());
+            
+            if (scoreTextFade != null)
+                StartCoroutine(_uiScreenEffects.DoTextFadeMoveDown(scoreTextFade));
+        }
+        
+        if (totalScore != null)
+        {
+            totalScore.text = _totalScore.ToString("00000000");
+        }
+    }
+    
+    private void PlayerScoreIncrease(int id, int score)
+    {
+        _playerScore[id] = score;
+
+        // if (_uiScreenEffects != null && subScoreEffect1 != null)
+        //     StartCoroutine(_uiScreenEffects.DoTextFade(subScoreEffect1));
+        // if (leftSubScoreImage != null)
+        //     StartCoroutine(_uiScreenEffects.DoImageFade(leftSubScoreImage));
+    }
+}

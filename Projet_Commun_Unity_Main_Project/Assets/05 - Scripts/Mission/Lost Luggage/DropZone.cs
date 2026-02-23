4fe1d354bfd92409872af86ac3587042c5f96a48
benjamin layson <benjaminlayson0@gmail.com>
2026-02-23 18:40:24 +0100

    update script dropzone

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Mission/Lost Luggage/DropZone.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Mission/Lost Luggage/DropZone.cs
@@ -17,21 +17,15 @@ public class DropZone : MonoBehaviour
     {
         if (!isActiveTarget) return;
 
-        // On essaie de récupérer ton composant Prop sur l'objet qui entre
         Prop valiseProp = other.GetComponent<Prop>();
 
-        // Si l'objet a bien le script Prop (donc c'est une valise ou un objet attrapable)
         if (valiseProp != null)
         {
-            // Optionnel : Tu peux même vérifier si c'est bien une valise grâce à ton enum !
-            // if (valiseProp.Type == PropType.RedLuggage || valiseProp.Type == PropType.BlueLuggage)
             
             Debug.Log($"Valise livrée ! Le joueur {valiseProp.OwnerId} marque un point !");
             
-            // On utilise TA fonction pour la détruire, ce qui va forcer le joueur à la lâcher proprement !
             valiseProp.Destroy(); 
             
-            // On prévient le GameManager de relancer la boucle
             LLGameManager.Instance.ValiseLivree();
         }
     }

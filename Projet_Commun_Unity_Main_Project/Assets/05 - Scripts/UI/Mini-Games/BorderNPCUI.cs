00ad11ef17e493756c715deea25ffc76b27e3d80
StylishBoi <max@rdh.ch>
2026-02-24 15:02:35 +0100

    - Added UI to AIBorder prefab

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/UI/Mini-Games/BorderNPCUI.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/UI/Mini-Games/BorderNPCUI.cs
@@ -5,14 +5,6 @@ using UnityEngine.InputSystem;
 
 public class BorderNPCUI : MonoBehaviour
 {
-    [Header("NPC UI")]
-    [SerializeField] GameObject triggerHud;
-    
-    [Header("UI Slots Elements")]
-    public List<Sprite> badItems;
-    public List<Sprite> goodItems;
-    public List<Image> slots;
-    
     [Header("UI Effect Elements")]
     [SerializeField] private Image happyImage;
     [SerializeField] private Image unhappyImage;
@@ -21,18 +13,9 @@ public class BorderNPCUI : MonoBehaviour
     
     [SerializeField] private float effectDuration=3f;
     
-    //Bool values
-    private bool _inRange = false;
-    private bool _resultTakenIn = false;
-    
     //Components
-    private PlayerInput _playerInput;
     private AudioManager _audioManager;
     private UIFeedback _uiFeedback;
-    
-    //Randomness for slots setup
-    private int _random;
-    private int _randomEvilSlot;
 
     void Start()
     {
@@ -42,90 +25,16 @@ public class BorderNPCUI : MonoBehaviour
             _uiFeedback.ImagePoolCreation(happyImage);
         }
         _audioManager = AudioManager.Instance;
-        _playerInput = FindAnyObjectByType<PlayerInput>();
-        
-        EvilNpcSetup();
-    }
-
-    void Update()
-    {
-        if (_inRange && _playerInput.actions["Interact"].triggered)
-        {
-            Debug.Log("Interact");
-            _resultTakenIn = true;
-            triggerHud.SetActive(false);
-            ArrestResult();
-        }
-        else if (_inRange && _playerInput.actions["Grab"].triggered)
-        {
-            Debug.Log("Grab");
-            _resultTakenIn = true;
-            triggerHud.SetActive(false);
-            CriminalResult();
-        }
-    }
-
-    void EvilNpcSetup()
-    {
-        for (int i = 0; i < slots.Count; i++)
-        {
-            _random = Random.Range(0, goodItems.Count);
-            slots[i].sprite = goodItems[_random];
-        }
-        _randomEvilSlot=Random.Range(0, slots.Count);
-        _random = Random.Range(0, badItems.Count);
-        slots[_randomEvilSlot].sprite = badItems[_random];
-    }
-
-    void GoodNpcSetup()
-    {
-        for (int i = 0; i < slots.Count; i++)
-        {
-            _random = Random.Range(0, goodItems.Count);
-            slots[i].sprite = goodItems[_random];
-        }
     }
     
-    public void HappyResult()
+    public void PlayFeedback(bool success, bool isEvil, bool accepted)
     {
-        AudioManager.Instance.PlaySfx(_audioManager.successSFX);
-        StartCoroutine(_uiFeedback.ImageFade(happyImage, effectDuration));
-    }
-    public void UnhappyResult()
-    {
-        _audioManager.PlaySfx(_audioManager.failureSFX);
-        StartCoroutine(_uiFeedback.ImageFade(unhappyImage, effectDuration));
-    }
-    public void ArrestResult()
-    {
-        _audioManager.PlaySfx(_audioManager.failureSFX);
-        StartCoroutine(_uiFeedback.ImageFade(arrestImage, effectDuration));
-    }
-    public void CriminalResult()
-    {
-        _audioManager.PlaySfx(_audioManager.successSFX);
-        StartCoroutine(_uiFeedback.ImageFade(criminalImage, effectDuration));
-    }
+        _audioManager?.PlaySfx(success ? _audioManager.successSFX : _audioManager.failureSFX);
+        
+        Image targetImage = accepted 
+            ? (isEvil ? criminalImage : happyImage) 
+            : (isEvil ? arrestImage : unhappyImage);
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Player" && !_resultTakenIn)
-        {
-            Debug.Log("Turning on trigger image");
-            _inRange = true;
-            EvilNpcSetup();
-            triggerHud.SetActive(true);
-            
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Player"&& !_resultTakenIn)
-        {
-            Debug.Log("Turning off trigger image");
-            _inRange = false;
-            triggerHud.SetActive(false);
-        }
+        StartCoroutine(_uiFeedback.ImageFade(targetImage, effectDuration));
     }
 }

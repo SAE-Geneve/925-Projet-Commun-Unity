766087dfed7f19e8c1bc07418b4d3231ebc737c1
Jemo <103411555+Jemow@users.noreply.github.com>
2026-03-01 20:06:04 +0100

    Fixed MissionTrigger collision for LostLuggage

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Mission/MissionManager.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Mission/MissionManager.cs
@@ -1,29 +1,30 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class MissionManager : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] private List<Mission> _randomMissions;
+    [SerializeField] private List<Mission> missions;
     [SerializeField] private Mission _finalMission;
 
     public void UnlockRandomMission()
     {
-        if (_randomMissions.Count <= 0)
+        if (missions.Count <= 0)
         {
             Debug.LogWarning("Random mission size is 0");
             return;
         }
         
-        int randomIndex = Random.Range(0, _randomMissions.Count);
+        int randomIndex = Random.Range(0, missions.Count);
         
-        _randomMissions[randomIndex].Unlock();
-        _randomMissions.RemoveAt(randomIndex);
+        missions[randomIndex].Unlock();
+        missions.RemoveAt(randomIndex);
     }
 
     public void TryUnlockFinalMission()
     {
-        if (_randomMissions.Count > 0) UnlockRandomMission();
+        if (missions.Count > 0) UnlockRandomMission();
         else _finalMission.Unlock();
     }
 }

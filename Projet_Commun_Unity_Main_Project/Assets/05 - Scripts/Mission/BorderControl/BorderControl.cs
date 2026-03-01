cc3e92376b9ec2a890eda795913fee520bdf9e45
hugo zeidan <hugzeidan@outlook.com>
2026-03-01 11:43:35 +0100

    added scripts to change minigames goals depending on player count

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Mission/BorderControl/BorderControl.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Mission/BorderControl/BorderControl.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 public class BorderControl : MonoBehaviour
 {
     [SerializeField] private GameObject addedConveyor1;
-    [SerializeField] private GameObject addedConveyor2;
     [SerializeField] private DirectionSwitch directionSwitch1;
+    
+    [SerializeField] private AIManagerBorder aiManager;
+    [SerializeField] private float[] spawnInterval = new float[4];
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
@@ -17,20 +19,25 @@ public class BorderControl : MonoBehaviour
     {
         switch (PlayerManager.Instance.Players.Count)
         {
+            case 1:
+                addedConveyor1.SetActive(false);
+                directionSwitch1.SetDirection(0);
+                aiManager.SetSpawnInterval(spawnInterval[0]);
+                break;
+            case 2:
+                addedConveyor1.SetActive(false);
+                directionSwitch1.SetDirection(0);
+                aiManager.SetSpawnInterval(spawnInterval[1]);
+                break;
             case 3:
                 addedConveyor1.SetActive(true);
-                if(addedConveyor2) addedConveyor2.SetActive(false);
                 directionSwitch1.SetDirection(1);
+                aiManager.SetSpawnInterval(spawnInterval[2]);
                 break;
             case 4:
                 addedConveyor1.SetActive(true);
-                if(addedConveyor2) addedConveyor2.SetActive(true);
                 directionSwitch1.SetDirection(1);
-                break;
-            default:
-                addedConveyor1.SetActive(false);
-                if(addedConveyor2) addedConveyor2.SetActive(false);
-                directionSwitch1.SetDirection(0);
+                aiManager.SetSpawnInterval(spawnInterval[3]);
                 break;
         }
     }

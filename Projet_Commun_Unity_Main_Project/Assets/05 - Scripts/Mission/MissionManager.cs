35c0c443b8cd6b901b46a3b31d569dad819b4e18
Jemo <103411555+Jemow@users.noreply.github.com>
2026-03-01 20:59:59 +0100

    Added mission introduction animation

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Mission/MissionManager.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Mission/MissionManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MissionManager : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private List<Mission> missions;
+    [SerializeField] private TextMeshProUGUI missionNameTmp;
+    [SerializeField] private Animator missionNameAnimator;
     
     [Header("Parameters")]
     [SerializeField] private float hubTime = 30f;
@@ -23,6 +26,9 @@ public class MissionManager : MonoBehaviour
         int randomIndex = Random.Range(0, missions.Count);
         
         missions[randomIndex].Unlock();
+        
+        missionNameTmp.SetText(missions[randomIndex].name);
+        missionNameAnimator.SetTrigger("Display");
     }
 
     public void OnMissionFinished() => StartCoroutine(HubTimeRoutine());

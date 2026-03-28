26c2b4b07193a6a767c33e43b6e475a485d9d1fa
StylishBoi <max@rdh.ch>
2026-03-28 17:09:44 +0100

    - Updated direction arrow pointer

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/UI/ArrowPointer.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/UI/ArrowPointer.cs
@@ -1,107 +1,62 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class ArrowPointer : MonoBehaviour
 {
-    [Header("Positioning")]
-    public List<Transform> missionPositions;
+    [Header("Positioning")] public List<Transform> missionPositions;
     //1 - Border Control
     //2 - Boarding
     //3 - Conveyor Belt
     //4 - Lost Luggage
     //This is hard-coded so poorly :crying_emoji:
-    [SerializeField] private RectTransform canvasTransform;
-    
-    [Header("Images")]
-    [SerializeField] private Sprite arrowSprite;
+
+    [Header("Images")] [SerializeField] private Sprite arrowSprite;
     [SerializeField] private Sprite crossSprite;
-    
+
     private Camera _mainCamera;
-    private MissionManager missionManager;
-    
-    [SerializeField] private Image _pointerImage;
-    [FormerlySerializedAs("_pointerRectTransform")] [SerializeField] private RectTransform pointerRectTransform;
-    
+    private MissionManager _missionManager;
+
+    [Header("Components")] [SerializeField]
+    private Image pointerImage;
+
+    [SerializeField] private RectTransform pointerRectTransform;
+    [SerializeField] private TextMeshProUGUI distanceText;
+
     private Vector3 _targetPosition;
 
     private void Start()
     {
         _mainCamera = Camera.main;
-        missionManager = FindObjectOfType<MissionManager>();
+        _missionManager = FindObjectOfType<MissionManager>();
     }
 
     private void Update()
     {
-        _pointerImage.gameObject.SetActive(!missionManager.currentMission.IsLocked);
+        pointerImage.gameObject.SetActive(!_missionManager.currentMission.IsLocked);
 
-        _targetPosition = missionManager.currentMission.ArrowTarget.position;
+        _targetPosition = _missionManager.currentMission.ArrowTarget.position;
 
-        // switch (missionManager.missionIndex)
-        // {
-        //     case 0:
-        //         _targetPosition = missionPositions[0].position;
-        //         break;
-        //     case 1:
-        //         _targetPosition = missionPositions[1].position;
-        //         break;
-        //     case 2:
-        //         _targetPosition = missionPositions[2].position;
-        //         break;
-        //     case 3:
-        //         _targetPosition = missionPositions[3].position;
-        //         break;
-        // }
-        
+        RotatePointerTowardsTarget();
+        CalculateDistance();
+    }
+
+    private void CalculateDistance()
+    {
         Vector3 targetPositionScreenPoint = _mainCamera.WorldToScreenPoint(_targetPosition);
         
-        //Checks if the target is in the screen
-        float borderSize = 150f;
-        bool isOffScreen = targetPositionScreenPoint.x < borderSize ||
-                           targetPositionScreenPoint.x > Screen.width - borderSize||
-                           targetPositionScreenPoint.y < borderSize+50 ||
-                           targetPositionScreenPoint.y > Screen.height - borderSize-50;
-
-        if (isOffScreen)
-        {
-            RotatePointerTowardsTarget();
-            _pointerImage.sprite = arrowSprite;
-            //Bring back the arrow in the screen if it is out of the border
-            Vector3 cappedTargetScreenPosition = targetPositionScreenPoint;
-            cappedTargetScreenPosition.x =
-                Mathf.Clamp(cappedTargetScreenPosition.x, borderSize, Screen.width - borderSize);
-            cappedTargetScreenPosition.y =
-                Mathf.Clamp(cappedTargetScreenPosition.y, borderSize, Screen.height - borderSize);
-
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                canvasTransform,
-                cappedTargetScreenPosition,
-                null,
-                out Vector2 localPos
-            );
-            pointerRectTransform.anchoredPosition = localPos;
-        }
-        else
-        {
-            _pointerImage.sprite = crossSprite;
-            //If on screen
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                canvasTransform,
-                targetPositionScreenPoint,
-                null,
-                out Vector2 localPos
-            );
-            pointerRectTransform.anchoredPosition = localPos;
-            pointerRectTransform.localEulerAngles = Vector3.zero;
-        }
+        float distance = Vector3.Distance(_mainCamera.transform.position, targetPositionScreenPoint)/10f;
+        
+        distanceText.text = (int)distance+"M";
     }
 
     private void RotatePointerTowardsTarget()
     {
         Vector3 targetPositionScreenPoint = _mainCamera.WorldToScreenPoint(_targetPosition);
-        
+
         Vector2 screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
+
 
         Vector2 direction = (Vector2)targetPositionScreenPoint - screenCenter;
         float angle = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) % 360;

d43501d82b5a31eecd6b158407195a4eb9ccfa17
WIXXEL <97680405+WixXelTV@users.noreply.github.com>
2026-04-06 01:02:18 +0200

    feat: add multi prop type to TriggerTask and trash bin mechanic to conveyor belt

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Game/Tasks/TriggerTask.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Game/Tasks/TriggerTask.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TriggerTask : GameTask
@@ -5,16 +6,23 @@ public class TriggerTask : GameTask
     [Header("Trigger Parameters")]
     [Tooltip("The prop type that needs to touch the task collider to succeed")]
     [SerializeField] protected PropType _propType = PropType.None;
+    [SerializeField] protected List<PropType> _acceptedTypes = new();
     [SerializeField] protected bool isDestroyed = true;
     [SerializeField] protected int score = 100;
 
     protected virtual void OnTriggerEnter(Collider other)
     {
-        if(Done || !other.TryGetComponent(out Prop prop) || prop.Type != _propType) return;
+        if (Done || !other.TryGetComponent(out Prop prop)) return;
+        if (!IsTypeAccepted(prop.Type)) return;
         OnTriggerValid(prop);
-
     }
-    
+
+    private bool IsTypeAccepted(PropType type)
+    {
+        if (_acceptedTypes.Count > 0) return _acceptedTypes.Contains(type);
+        return type == _propType;
+    }
+
     public void SetPropType(PropType propType) => _propType = propType;
 
     protected virtual void OnTriggerValid(Prop prop)
@@ -23,14 +31,14 @@ public class TriggerTask : GameTask
         if (playerController == null && PlayerManager.Instance != null)
             playerController = PlayerManager.Instance.Players.Find(p => p.Id == prop.OwnerId);
         Succeed(playerController);
-        
+
         GameManager gm = GameManager.Instance;
-        
+
         if (gm)
         {
             if (gm.CurrentMission != null) gm.Scores.AddMissionScore(score, prop.OwnerId);
             else gm.Scores.AddPlayerScore(score, prop.OwnerId);
         }
-        if(isDestroyed) prop.Destroy();
+        if (isDestroyed) prop.Destroy();
     }
 }

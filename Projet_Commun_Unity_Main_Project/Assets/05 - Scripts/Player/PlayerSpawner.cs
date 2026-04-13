845f649e1ad49e9aa9155fc1b28ae88c502095a9
hugo zeidan <hugzeidan@outlook.com>
2026-04-13 11:35:05 +0200

    applied code review for player teleportation

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Player/PlayerSpawner.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Player/PlayerSpawner.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerSpawner : MonoBehaviour
@@ -10,32 +11,27 @@ public class PlayerSpawner : MonoBehaviour
     [SerializeField] private bool _spawnOnStart;
     [SerializeField] private bool _useSpawnRotation;
 
+    private List<PlayerController> _players;
+
     private void Start()
     {
         if(_spawnOnStart) Spawn();
+
+        _players = PlayerManager.Instance.Players;
     }
     
     public void Spawn()
     {
-        for (int i = 0; i < PlayerManager.Instance.Players.Count; i++)
+        for (int i = 0; i < _players.Count; i++)
         {
-            if (PlayerManager.Instance.Players[i].Ragdoll.IsRagdoll)
-            {
-                StartCoroutine(WaitRagdollCoroutine(i));
-            }
-            else
-            {
-                Transform playerTransform = PlayerManager.Instance.Players[i].transform;
-                playerTransform.position = _spawnPoints[i].position;
-                if(_useSpawnRotation) playerTransform.rotation = _spawnPoints[i].rotation;
-            }
+            StartCoroutine(WaitRagdollCoroutine(i));
         }
     }
 
     private IEnumerator WaitRagdollCoroutine(int idx)
     {
-        yield return new WaitUntil(() => !PlayerManager.Instance.Players[idx].Ragdoll.IsRagdoll);
-        Transform playerTransform = PlayerManager.Instance.Players[idx].transform;
+        yield return new WaitUntil(() => !_players[idx].Ragdoll.IsRagdoll);
+        Transform playerTransform = _players[idx].transform;
         playerTransform.position = _spawnPoints[idx].position;
         if(_useSpawnRotation) playerTransform.rotation = _spawnPoints[idx].rotation;
     }

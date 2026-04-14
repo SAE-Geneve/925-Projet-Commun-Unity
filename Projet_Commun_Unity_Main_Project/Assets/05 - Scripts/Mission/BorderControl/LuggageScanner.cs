74fbf9e10c93963f4adaafde564536bc926e2fea
WIXXEL <97680405+WixXelTV@users.noreply.github.com>
2026-04-14 16:13:52 +0200

    feat: add visual and audio feedbacks to luggage scanner

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Mission/BorderControl/LuggageScanner.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Mission/BorderControl/LuggageScanner.cs
@@ -3,6 +3,11 @@ using UnityEngine.Events;
 
 public class LuggageScanner : MonoBehaviour
 {
+    [Header("Feedbacks")]
+    [SerializeField] private Animator alarmAnimator;
+    [SerializeField] private AudioSource audioSource; 
+    [SerializeField] private AudioClip beepSound;
+
     [Header("Events")] 
     [SerializeField] private UnityEvent onGoodProp;
     [SerializeField] private UnityEvent onBadProp;
@@ -11,7 +16,15 @@ public class LuggageScanner : MonoBehaviour
     {
         if(!other.CompareTag("Prop") || !other.TryGetComponent(out Prop prop)) return;
         
+        PlayScanFeedbacks();
+        
         if (prop.Type == PropType.GoodProp) onGoodProp.Invoke();
         else if(prop.Type == PropType.BadProp) onBadProp.Invoke();
+    }
+
+    private void PlayScanFeedbacks()
+    {
+        if (alarmAnimator) alarmAnimator.SetTrigger("Alert");
+        if (audioSource && beepSound) audioSource.PlayOneShot(beepSound);
     }
 }

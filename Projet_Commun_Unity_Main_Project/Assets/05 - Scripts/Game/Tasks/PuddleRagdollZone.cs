cfa21e08c059df5cd288e3bde382fdd678742fa6
hugo zeidan <hugzeidan@outlook.com>
2026-04-16 14:18:44 +0200

    added delay on the puddles ragdoll

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Game/Tasks/PuddleRagdollZone.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/Game/Tasks/PuddleRagdollZone.cs
@@ -1,9 +1,21 @@
+using System.Collections;
 using UnityEngine;
 
 public class PuddleRagdollZone : MonoBehaviour
 {
+    [SerializeField] private float waitBeforeRagdoll = 0.5f;
+    
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent(out Ragdoll ragdoll)) ragdoll.RagdollOn();
+        if (other.TryGetComponent(out Ragdoll ragdoll))
+        {
+            StartCoroutine(WaitBeforeRagdoll(ragdoll));
+        }
+    }
+
+    private IEnumerator WaitBeforeRagdoll(Ragdoll ragdoll)
+    {
+        yield return new WaitForSeconds(waitBeforeRagdoll);
+        ragdoll.RagdollOn();
     }
 }

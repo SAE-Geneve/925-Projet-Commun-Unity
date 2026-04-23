c61da77398032d9f607e12fd3e16e10f429078fa
WIXXEL <97680405+WixXelTV@users.noreply.github.com>
2026-04-23 15:12:44 +0200

    Added ui icon on trashcans and trashs

--- Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/UI/Universal/UIFaceCamera.cs
+++ Projet_Commun_Unity_Main_Project/Assets/05 - Scripts/UI/Universal/UIFaceCamera.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
 
-public class UIFaceCamera : MonoBehaviour
+public class UIFollowAndFaceCamera : MonoBehaviour
 {
+    [Header("Réglages du Ballon")]
+    [Tooltip("L'objet qui tourne (Le chien)")]
+    [SerializeField] private Transform _target; 
+    
+    [Tooltip("La distance vers le haut (La ficelle)")]
+    [SerializeField] private Vector3 _offset = new Vector3(0, 2f, 0); 
+
     private Transform _cameraTransform;
 
-    private void Awake() => _cameraTransform = Camera.main.transform;
-
-    private void Update()
+    private void Awake() 
     {
-        transform.rotation = _cameraTransform.transform.rotation;
+        _cameraTransform = Camera.main.transform;
+    }
+
+    // On utilise LateUpdate plutôt que Update pour éviter les tremblements !
+    private void LateUpdate()
+    {
+        // Sécurité : si on a oublié de mettre une cible, on arrête tout
+        if (_target == null) return;
+
+        // 1. Position : On se place SUR la cible, et on ajoute notre décalage vers le haut
+        transform.position = _target.position + _offset;
+
+        // 2. Rotation : On regarde toujours la caméra
+        transform.rotation = _cameraTransform.rotation;
     }
 }
